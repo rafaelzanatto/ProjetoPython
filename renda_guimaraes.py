c680ae428180335e2aae0f20a5c4f37442d72e0d
rafaelzanatto <rafaelzanattos@gmail.com>
2026-06-04 20:17:29 +0100

    renda_guimaraes
    adc alguns detalhes, mas consegui finalizar. Treino concluído

---
+++ renda_guimaraes.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import io
+dados = '''concelho,ano,ganho_medio_mensal_euro
+Guimarães,2023,1247
+Braga,2023,1321
+Famalicão,2023,1198
+Vizela,2023,1156
+Fafe,2023,1089
+Porto,2023,1463
+Lisboa,2023,1687
+Coimbra,2023,1225'''
+
+df = pd.read_csv(io.StringIO(dados))
+print(df)
+print("\n--- ESTATÍSTICAS ---")
+print(df['ganho_medio_mensal_euro'].describe())
+
+import matplotlib.pyplot as plt
+##grafico de barras
+plt.bar(df['concelho'], df['ganho_medio_mensal_euro'], color='red')
+plt.xlabel('Concelho')
+plt.ylabel('Ganho Médio Mensal (Euro)')
+plt.title('Renda Média Mensal por Concelho')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig('renda_mensal_concelhos.png', dpi=300, bbox_inches='tight') ##salvar o gráfico como imagem em alta qualidade
+plt.show() ##continua mostrando o gráfico na tela
